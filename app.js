ed50114e18867a63c338e9583db0a56a3ba3e4aa
Frank Solleveld <fsolleveld@outlook.com>
2018-12-10 11:45:51 +0100

    Error handling, tutorial part 4

--- app.js
+++ app.js
@@ -4,10 +4,32 @@ const express = require('express');
 // Here we initiate Express
 const app = express();
 
+// Here we initiate morgan
+const morgan = require('morgan');
+
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+app.use(morgan('dev'));
+
+// Routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+// Error handling is programmed below
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
