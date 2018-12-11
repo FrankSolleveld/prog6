3d9f499d901d7b236e47d139c70ec4963594b2ce
Frank Solleveld <fsolleveld@outlook.com>
2018-12-11 13:38:51 +0100

    Changed OPTIONS at app.js

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use((req, res, next) => {
     );
     if (req.method === 'OPTIONS'){
         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        res.header('Allow', 'POST, GET, OPTIONS');
         return res.status(200).json({});
     }
     next();
