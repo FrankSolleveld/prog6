fbf64237b7841a7b7b15515df9804f9e7cf594e8
Frank Solleveld <fsolleveld@outlook.com>
2018-12-11 20:38:04 +0100

    Final commit for setting up db and adding patch, get, delete code in products.js, tutorial part 6 3/2 (Sorry bout that)

--- api/routes/products.js
+++ api/routes/products.js
@@ -6,8 +6,23 @@ const mongoose = require('mongoose');
 const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message:'Handling GET requests to /products'
+    Product.find()
+    .exec()
+    .then(docs => {
+        console.log(docs);
+        if (docs.length>=0){
+            res.status(200).json(docs);
+        } else {
+            res.status(404).json({
+                message: "No entries found."
+            });
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error:err
+        })
     });
 });
 
@@ -25,11 +40,13 @@ router.post('/', (req, res, next) => {
         .catch(err => console.log(err));
         res.status(201).json({
             message:'Handling POST requests to /products',
-            createdProduct: product
+            createdProduct: result
         });
     }).catch(err =>{
-        console.log('blaaaa');
         console.log(err);
+        res.status(500).json({
+            error: err
+        });
     });
     
 });
@@ -37,28 +54,64 @@ router.post('/', (req, res, next) => {
 // URI /products/productId -> check Postman
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
-    if (id === 'special'){
-        res.status(200).json({
-            message: 'You discovered the special ID',
-            id: id
-        });
-    } else {
-        res.status(200).json({
-            message: 'You passed an ID',
-            id: id
-        })
-    }
+    Product.findById(id)
+    .exec()
+    .then(doc => {
+        console.log("From db: " + doc);
+        if(doc){
+            res.status(200).json(doc);
+        } else {
+            res.status(404).json({
+                message: 'Invalid entry for provided ID.'
+            });
+        };
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({error: err});
+    });
 });
 
 router.patch('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Updated product'
+    const id = req.params.productId;
+    // This will have the data that needs to be patched. In the for loop the data is being retrieved from the body and put in the empty model updateOps.
+    const updateOps = {};
+    for (const ops of req.body){
+        updateOps[ops.propName] = ops.value;
+    }
+    Product.update({_id: id}, { $set:updateOps })
+    .exec()
+    .then(result => {
+        console.log(result);
+        res.status(200).json(result);
     })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
 });
 
+/*
+How to PATCH:
+[
+	{"propName":"name", "value":"Harry Potter 6"}	
+]
+*/
+
 router.delete('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Deleted product'
+    const id = req.params.productId;
+    Product.remove({_id: id})
+    .exec()
+    .then(result => {
+        res.status(200).json(result);
     })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
 });
 module.exports = router;
